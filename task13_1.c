074929f410000f1d1e896581eb780f6cc4023580
LyapunKate <liapun.ev@phystech.edu>
2021-12-14 14:00:24 +0300

    fixed

--- task13_1.c
+++ task13_1.c
@@ -1,49 +1,72 @@
+//программа порождает дочерний процесс, ждёт его завершения
+//выводит информацию о завершении
+//
+//Функция waitpid приостанавливает выполнение текущего процесса до тех пор, 
+//пока не появится сигнал, который завершит текущий процесс, 
+//либо потребует вызвать функцию обработчик
+//Она используется для ожидания изменения состояния процесса-потомка
+//pid_t waitpid(pid_t pid, int *status, int options);
+//с помощью options задаём, чтобы он ожидал не только прекращения работы процесса
+
+#define _GNU_SOURCE
+
 #include <sys/types.h>
 #include <unistd.h>
-#include <wait.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-
-// функция обрабатывает сигнал SIGCHILD 
-void my_handler(int nsig) {
-	int status;
-	pid_t pid;
-	//узнаём статус и идентификатор завершившегося процесса
-	if ((pid = waitpid(-1, &status, 0)) < 0) {
-		// печатаем в случае ошибки и продолжаем работу
-		printf ("Some error on waitpid errno = %d\n", errno);
-	}
-	else {
-		// если всё хорошо, то анализируем статус завершившегося процесса
-		if ((status & 0xff) == 0) {
-			// процесс заверщился с вызовом функции exit
-			printf("Process %d was exited with status %d\n", pid, status >> 8);
-	}
-		else if ((status & 0xff00) == 0) {
-			// процесс завершён с помощью сигнала
-		       printf("Process %d killed by signal %d %s\n", pid, status &0x7f, (status & 0x80) ?
-			       "with core file" : "without core file");
-		}
-	}
-}
+#include <sys/stat.h>
+#include <string.h>
 
 
 int main (void) {
-	pid_t pid, ppid;
-	(void) signal (SIGCHLD, my_handler);
+	pid_t pid;
 	// порождаем дочерний процесс
+	// выходим в случае ошибки
 	if ((pid = fork()) < 0) {
-		printf("Can\'t fork child 1\n");
-		exit(1);
+		perror("Can't fork child");
+		return 1;
 	}
-	else if (pid == 0) {
-		exit(100);
-		// дочерний процесс завершается с кодом 200
+	//pid == 0 при вызове pid = fork(), когда это дочерний процесс
+	if (pid == 0) {
+		pid_t child = getpid();
+		printf("This is child process PID = %d\n", child);
+		sleep(10);
 	}
+	//это в родительском процессе
+	else {
+		int status = 0;
+		//узнаём статус и идентификатор завершившегося процесса
+       		if ((waitpid(pid, &status, WUNTRACED | WCONTINUED)) < 0) {
+                // печатаем в случае ошибки и продолжаем работу
+                printf ("Some error on waitpid errno = %d\n", errno);
+		}
+		//WIFEXITED не равно нулю, если дочерний процесс успешно заверщился
+		//WIFSIGNALED возвращает истинное значение, если дочерний процесс завершился из-за необработанного сигнала
+		//пока дочерний процесс не завершился выполняем
+		while (WIFEXITED(status) == 0 && WIFSIGNALED(status) == 0) {
+			//возвращает истинное значение, если потомок остановлен по сигналу
+			if(WIFSTOPPED(status)) {
+				printf("Child process PID %d was stopped by signal %d (%s)\n", pid, WSTOPSIG(status), strsignal(WTERMSIG(status)));
+			}
+			//возвращает истинное значение, если потомок продолжил работу по сигналу SIGCONT
+			if(WIFCONTINUED(status)) {
+				printf("Child process PID %d continue to work\n", pid);
+			}
+		}
+		//WIFEXITED возвращает истинное значение, если потомок нормально завершился
+		if(WIFEXITED(status)) {
+			printf("Child process PID %d exited with code %d\n", pid, WEXITSTATUS(status));
+		}
+		//WIFSIGNALED возвращает истинное значение, если потомок завершился из-за сигнала
+		else if(WIFSIGNALED(status)) {
+			int child_signal = WTERMSIG(status);
+			printf("Child process PID %d was killed by signal %d (%s)", pid, child_signal, strsignal(child_signal));
+			}
+		}
 
-	sleep (5000);
 	return 0;
 }	
 
