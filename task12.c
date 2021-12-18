9e1c02cb76a87c463ae7c69a0b13589e10a31a0a
LyapunKate <liapun.ev@phystech.edu>
2021-12-18 20:39:35 +0300

    now it works, almost new code

--- task12.c
+++ task12.c
@@ -1,69 +1,111 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <string.h>
-
-void print_cpu_time()
-{
-	struct rusage usage;
-	getrusage (RUSAGE_SELF, &usage);
-	printf ("CPU time: %ld.%06ld sec user, %ld.%06ld sec system\n", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec, usage.ru_stime.tv_sec, usage.ru_stime.tv_usec);
-}
-
-/*Prints the argument list, one argument to a line, 
- * of the process given by PID. */
-
-void print_process_arg_list (pid_t pid)
-{
-	int fd;
-	char filename[24];
-	char arg_list[1024];
-	size_t length;
-	char* next_arg;
-
-	/* Generate the name of the cmdline file fro the process */
-
-	snprintf (filename, sizeof(filename), "/proc/%d/cmdline", (int)pid);
-
-	/*Read the contents of the file. */
-
-	fd = open(filename, O_RDONLY);
-	length = read (fd, arg_list, sizeof (arg_list));
-	close (fd);
-
-	/* Add ending symbol */
-
-	arg_list[length] = '\0';
-
-	/* Loop over arguments. Arguments re separated by NULLs. */
-
-	next_arg = arg_list;
-
-	while (next_arg < arg_list +length) {
-		/* Print the argument. Each is NUL-terminated, 
-		 * that is why we can separate them as a string */
-		printf ("%s\n", next_arg);
-		/* As all arguments are NULL-separated,
-		 * the length of next_argument is not the length of the string
-		 * so we need to count the lenghth of the string */
-		next_arg += strlen (next_arg) + 1;
-	}
-}
 
 
-int main(int argc, char* argv[])
-{
-	printf("The process ID is %d\n", (int) getpid());
-	printf("The parent process is %d\n", (int) getppid());
-	pid_t pid = (pid_t) atoi (argv[1]);
-	print_process_arg_list (pid);
-	print_cpu_time();
+#define NGROUPS 20
 
-	return 0;
+int main(void) {
+    // UID and user name
+    
 
+    // PID, PPID
+    // Each process has a unique non-negative integer identifier that is assigned when the process is created using fork -- PID
+    // A process's parent process ID identifies the process thet created this process using fork(2) -- PPID
+    pid_t process_id = getpid();//These functions are always successful too.
+    pid_t parent_pid = getppid();
+    printf("Process ID: %d\nParent's process ID: %d\n", process_id, parent_pid);
+    //Process Group ID and Session ID
+    pid_t process_group_id = getpgrp();
+    pid_t session_id = getsid(process_id);
+    printf("Process Group ID: %d\nSession ID: %d\n", process_group_id, session_id);
+
+    //User and Group Identifiers
+    //Real user ID and real group ID. These ID's determine who owns the process
+
+    pid_t uid = getuid();
+    pid_t gid = getgid();
+    const char* grp_name;
+    struct group* grp_info = getgrgid(getpgid(process_id));
+    if(grp_info == NULL) {
+        grp_name = "?";
+    } else {
+        grp_name = grp_info->gr_name;
+    }
+    printf("Group ID = %d, %s\n", gid, grp_name);
+
+    // get user name
+    struct passwd* pass_info;
+    const char* user_name;
+    pass_info = getpwuid(getuid());
+    if(pass_info == NULL) {
+        user_name = "?";
+    } else {
+        user_name = pass_info->pw_name;
+    }
+    printf("User ID = %d, %s\n", uid, user_name);
+
+    //Effective user ID and effective group ID
+    
+    printf("Effective user ID = %d\n", geteuid());
+    printf("Effective group ID = %d\n", getegid());
+ 
+
+    // all groups
+    int j, ngroups;
+    gid_t *groups;
+    struct passwd *pw;
+    struct group *gr;
+    ngroups = NGROUPS;
+    groups = calloc(ngroups, sizeof (gid_t));
+    if (groups == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    //returns a pointer to a structure with passwords
+    pw = getpwnam(user_name);
+    if (pw == NULL) {
+        perror("getpwnam");
+        exit(EXIT_SUCCESS);
+    }
+    // get list of groups to which a user belongs
+    if (getgrouplist(user_name, pw->pw_gid, groups, &ngroups) == -1) {
+        fprintf(stderr, "getgrouplist() вернула -1; ngroups = %d\n",
+                ngroups);
+        exit(EXIT_FAILURE);
+    }
+    //print list of groups with names
+    for (j = 0; j < ngroups; j++) {
+        printf("Group (name): %d", groups[j]);
+        gr = getgrgid(groups[j]);
+        if (gr != NULL) {
+            printf(" (%s)", gr->gr_name);
+        }
+        printf("\n");
+    }
+
+    //set file mode creation mask
+    mode_t u_mask = umask(ALLPERMS);
+    printf("Umask: %o\n", u_mask);
+    //umask sets the calling process's file mode creation mask to mask&0777
+    //(only the file permissions bits of mask are used)
+    //returns the previous value of the mask
+    //thats why we need to return the previous value 
+    u_mask = umask(u_mask);
+
+    //get sheduling priority
+    int pr = getpriority(PRIO_PROCESS, 0);
+    if(pr == -1) {
+        perror("Failure getpriority");
+        exit(EXIT_FAILURE);
+    }
+    printf("Sheduling priority: %d\n", pr);
+    return 0;
 }
